6e5af147a9f282107c85a8ecc885bdac8e505040
sairiinxw <cyrenexwang@gmail.com>
2025-10-16 08:22:00 -0700

    HTML bug

--- initial4.py
+++ initial4.py
@@ -1,5 +1,7 @@
 import csv
 import requests
+from requests_html import HTMLSession
+from lxml_html_clean import clean_html
 
 #LAST RUN: 10/14/25 2:03 PM PST
 #scraping text output file: output20.txt
@@ -16,9 +18,10 @@ Parameters:
 
 newspaperName = 'O Globo'
 page = 1 # initial page
+last_page = 10 # last page scraped
 url = "https://oglobo.globo.com/ultimas-noticias/index/feed/pagina-" + str(page) + ".ghtml" # most recent articles on oglobo
 csv_output_file = 'clean_articles20.csv'
-scrapeNum = 100 # number of articles you want to scrape
+scrapeNum = 5000 # number of articles you want to scrape
 
 
 count = 0 # initial count of scraped articles per page
@@ -33,11 +36,17 @@ try:
             where 'h2' accesses these blocks in the html
         results (Object array): stores the newsarticle objects i.e. cleaned articles with names, links, and article content
     """
-    # session = HTMLSession()
-    response = requests.get(url)
-    html = response.text
-    articles = html.split("h2")
-
+    session = HTMLSession()
+    response = session.get(url)
+    # response.html.render(sleep=1, scrolldown=100)
+    html = ""
+    for page in range(last_page + 1):
+        url = "https://oglobo.globo.com/ultimas-noticias/index/feed/pagina-" + str(page) # most recent articles on oglobo
+        print(f"Current Page Number:" + str(page))
+        response = requests.get(url)
+        html = html + response.text
+    articles = clean_html(html).split("h2")
     print(articles)
+    
 except requests.exceptions.RequestException as e:
     print(e)
---
+++ test4.sh
@@ -0,0 +1,5 @@
+set -e
+
+export LC_ALL="en_US.UTF-8"
+
+python3 initial4.py > output20.txt
